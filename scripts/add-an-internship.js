4f58c8dd8c0e19c525ba2e7d8eaf3910ee6d1dd8
Mathis <mathis.lara@icloud.com>
2024-07-02 17:48:29 +0200

    Login and register

--- scripts/add-an-internship.js
+++ scripts/add-an-internship.js
@@ -7,4 +7,52 @@ function renderTable(){
     let tableHTML = '';
 
     tableArray.forEach((tableObject, index) => {
-        const {role, company, salary} = tableObj
+        const {role, company, salary} = tableObject;
+        const html = `
+                    <div class="table-row">
+                        <div class="table-column-role">${role}</div>
+                        <div class="table-column-company">${company}</div>
+                        <div class="table-column-salary">${salary}</div>
+                        <div class="table-column-state">Not answered</div>
+                        <div class="delete-button-container"><button class="delete-button">X</button></div>
+                    </div>
+
+                    <div class="hr-container"><hr class="horizontal-line"></hr></div>
+                    
+            `;
+        tableHTML += html;
+    });
+
+    document.querySelector('.table').innerHTML = tableHTML;
+
+    document.querySelectorAll('.delete-button').forEach((deleteButton, index) => {
+        deleteButton.addEventListener('click', () => {
+            tableArray.splice(index, 1);
+            renderTable();
+        })
+    })
+}
+
+
+document.querySelector('.add-button').addEventListener('click', () => {
+    addRow();
+    localStorage.setItem('table', JSON.stringify(tableArray));
+});
+
+const roleInput = document.querySelector('.role-input');
+const role = roleInput.value;
+
+const companyInput = document.querySelector('.company-input');
+const company = companyInput.value;
+
+const salaryInput = document.querySelector('.salary-input');
+const salary = salaryInput.value;
+
+
+function addRow(){
+    
+    tableArray.push({role, company, salary});
+    renderTable();
+
+}
+
--- server.js
+++ server.js
@@ -1,12 +1,151 @@
 const express = require('express');
 const app = express();
+const { pool } = require('./dbConfig.js');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
+const flash = require('express-flash');
+const passport = require('passport');
+
+
+const initializePassport = require("./passportCOnfig.js");
+
+initializePassport(passport);
+
 
 const PORT = process.env.PORT || 4000;
 
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({
+    secret: 'secret',
+
+    resave: false,
+
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(flash());
+
 app.get('/', (req, res) => {
-    res.send('Hello')
+    res.render('index');
 });
+
+app.get('/users/register', checkAuthenticated, (req, res)=>{
+    res.render('register');
+});
+
+app.get('/users/login', checkAuthenticated, (req, res)=>{
+    res.render('login');
+});
+
+app.get('/users/dashboard', checkNotAuthenticated, (req, res)=>{
+    console.log(req.isAuthenticated());
+    res.render('dashboard', { user: req.user.name });
+});
+
+app.get('/users/logout', (req, res)=>{
+    req.logout((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/users/login');
+        req.flash('success_msg', 'You have successfully logged out')
+    });
+})
+
+app.post('/users/register', async (req, res)=>{
+    let {name, email, password, password2} = req.body;
+    console.log({
+        name, 
+        email, 
+        password, 
+        password2
+    });
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2){
+        errors.push({message : "Please enter all fields"});
+    }
+
+    if(password.length < 6){
+        errors.push({message : "Password should be at least 6 characters"});
+    }
+
+    if(password != password2){
+        errors.push({message : "Passwords do not match"});
+    }
+
+    if(errors.length > 0){
+        res.render('register', {errors});
+    } else {
+        let hashedPassword = await bcrypt.hash(password, 10);
+        console.log(hashedPassword);
+
+        pool.query(
+            `SELECT * FROM users
+              WHERE email = $1`,
+            [email],
+            (err, results) => {
+              if (err) {
+                console.log(err);
+              }
+              console.log(results.rows);
+      
+              if(results.rows.length>0){
+                errors.push({message: "Email already registered"});
+                res.render('register', { errors });
+              }else{
+                pool.query(
+                    `INSERT INTO users (name, email, password)
+                    VALUES ($1, $2, $3)
+                    RETURNING id, password`, [name, email, hashedPassword],
+                    (err, results) => {
+                        if (err){
+                            throw err
+                        }
+                        console.log(results.rows);
+                        req.flash('success_msg', "You are now registered. Please log in");
+                        res.redirect('/users/login');
+                    }
+
+                )
+              }
+            }
+        );
+    }
+});
+
+app.post(
+    '/users/login', 
+    passport.authenticate('local', {
+        successRedirect: "/users/dashboard",
+        failureRedirect: "/users/login",
+        failureFlash: true,
+}));
+
+function checkAuthenticated(req, res, next){
+    if (req.isAuthenticated()){
+        return res.redirect('/users/dashboard');
+    }else{
+        next();
+    }
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    res.redirect('/users/login');
+}
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
+
